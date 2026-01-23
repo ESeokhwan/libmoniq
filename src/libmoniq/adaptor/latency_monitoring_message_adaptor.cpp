bbd6ea6ad8b46f063d138bff87de75074a53e4e1
ESeokhwan <huan99@hanmail.net>
2026-01-23 09:05:57 +0900

    feat: add fast json message adaptor by detaching payload from json

--- src/libmoniq/adaptor/latency_monitoring_message_adaptor.cpp
+++ src/libmoniq/adaptor/latency_monitoring_message_adaptor.cpp
@@ -102,5 +102,109 @@ std::string JsonBasedLatencyMonitoringMessageGenerator::get_random_payload() {
     return payload.str();
 }
 
+std::string FastJsonBasedLatencyMonitoringMessageAdaptor::generate(std::string messageId) {
+    auto now = std::chrono::system_clock::now();
+    auto nanosec = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch());
+    double millsec = nanosec.count() / 1e6;
+    return generate(messageId, millsec);
+}
+
+std::string FastJsonBasedLatencyMonitoringMessageAdaptor::generate(std::string messageId, double requested_at) {
+    json messageJson = {
+        {ID_KEY_, messageId},
+        {REQUESTED_AT_KEY, requested_at}
+    };
+    return messageJson.dump() + DIV_CHAR + get_random_payload();
+}
+
+std::string FastJsonBasedLatencyMonitoringMessageAdaptor::generate(std::string messageId, double requested_at, std::map<std::string, std::string> oth_kvs) {
+    json messageJson = {
+        {ID_KEY_, messageId},
+        {REQUESTED_AT_KEY, requested_at}
+    };
+    for (const auto& [key, value] : oth_kvs) {
+        messageJson[key] = value;
+    }
+    return messageJson.dump() + DIV_CHAR + get_random_payload();
+}
+
+std::string FastJsonBasedLatencyMonitoringMessageAdaptor::extract_content(const std::string& message) const {
+    std::string payload_removed = get_payload_removed(message);
+
+    try {
+        json message_json = json::parse(payload_removed);
+        if (message_json.contains(ID_KEY_)) return message_json[ID_KEY_];
+    } catch (...) {
+        return "";
+    }
+    return "";
+}
+
+double FastJsonBasedLatencyMonitoringMessageAdaptor::extract_requested_at(const std::string& message) const {
+    std::string payload_removed = get_payload_removed(message);
+
+    try {
+        json messageJson = json::parse(payload_removed);
+        if (messageJson.contains(REQUESTED_AT_KEY)) {
+            if (messageJson[REQUESTED_AT_KEY].is_number()) return messageJson[REQUESTED_AT_KEY].get<int64_t>();
+            if (messageJson[REQUESTED_AT_KEY].is_string()) return std::stoll(messageJson[REQUESTED_AT_KEY].get<std::string>());
+        }
+    } catch (...) {
+        return -1;
+    }
+    return -1;
+}
+
+std::string FastJsonBasedLatencyMonitoringMessageAdaptor::extract_other_kvs(const std::string& message, const std::string& key) const {
+    std::string payload_removed = get_payload_removed(message);
+
+    try {
+        json messageJson = json::parse(payload_removed);
+        if (messageJson.contains(key)) return messageJson[key];
+    } catch (...) {
+        return "";
+    }
+    return "";
+}
+
+std::string FastJsonBasedLatencyMonitoringMessageAdaptor::get_payload_removed(const std::string& message) const {
+    size_t content_size = message.rfind(DIV_CHAR);
+    if (content_size == std::string::npos) {
+        content_size = message.length();
+    }
+    return message.substr(0, content_size);
+}
+
+FastJsonBasedLatencyMonitoringMessageGenerator::FastJsonBasedLatencyMonitoringMessageGenerator(int payload_size, int pre_indices_size)
+    : FastJsonBasedLatencyMonitoringMessageAdaptor(), payload_size_(payload_size), cur_idx_(0) {
+    init_(pre_indices_size);
+}
+
+void FastJsonBasedLatencyMonitoringMessageGenerator::init_(int pre_indices_size) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, static_cast<int>(PAYLOAD_CHARACTERS_.size()) - 1);
+
+    pre_generated_indices_.resize(pre_indices_size);
+    cur_idx_ = 0;
+    for (int& idx : pre_generated_indices_) {
+        idx = dist(gen);
+    }
+}
+
+std::string FastJsonBasedLatencyMonitoringMessageGenerator::get_random_payload() {
+    std::ostringstream payload;
+    for (int i = 0; i < payload_size_; i++) {
+        int pre_generated_size = static_cast<int>(pre_generated_indices_.size());
+        if (cur_idx_ >= pre_generated_size * MAX_CUR_IDX_MULTIPLIER_) {
+            cur_idx_ = 0;
+        }
+        char random_char = PAYLOAD_CHARACTERS_[pre_generated_indices_[cur_idx_ % pre_generated_size]];
+        payload << random_char;
+        cur_idx_ += 1;
+    }
+    return payload.str();
+}
+
 }
 }
--- src/libmoniq/adaptor/latency_monitoring_message_adaptor.h
+++ src/libmoniq/adaptor/latency_monitoring_message_adaptor.h
@@ -73,5 +73,60 @@ private:
     void init_(int pre_indices_size);
 };
 
+class FastJsonBasedLatencyMonitoringMessageAdaptor: public ILatencyMonitoringMessageAdaptor {
+protected:
+    static constexpr std::string_view ID_KEY_ = "id";
+    static constexpr std::string_view REQUESTED_AT_KEY = "requested_at";
+    static constexpr char DIV_CHAR = '!';
+
+    virtual std::string get_random_payload() = 0;
+    virtual std::string get_payload_removed(const std::string& message) const;
+
+public:
+    std::string generate(std::string message_id) override;
+
+    std::string generate(std::string messageId, double requested_at) override;
+
+    virtual std::string generate(std::string messageId, double requested_at, std::map<std::string, std::string> oth_kvs);
+
+    std::string extract_content(const std::string& message) const override;
+
+    double extract_requested_at(const std::string& message) const override;
+
+    virtual std::string extract_other_kvs(const std::string& message, const std::string& key) const;
+};
+
+
+class FastExtractOnlyJsonBasedLatencyMonitoringMessageAdaptor: public FastJsonBasedLatencyMonitoringMessageAdaptor {
+public:
+    explicit FastExtractOnlyJsonBasedLatencyMonitoringMessageAdaptor() {}
+
+protected:
+    std::string get_random_payload() override {
+        throw ImproperUsageException();
+    }
+};
+
+
+class FastJsonBasedLatencyMonitoringMessageGenerator: public FastJsonBasedLatencyMonitoringMessageAdaptor {
+public:
+    FastJsonBasedLatencyMonitoringMessageGenerator(int payload_size, int pre_indices_size);
+
+    ~FastJsonBasedLatencyMonitoringMessageGenerator() = default;
+
+protected:
+    std::string get_random_payload();
+
+private:
+    static constexpr std::string_view PAYLOAD_CHARACTERS_ = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    static constexpr int MAX_CUR_IDX_MULTIPLIER_ = 10;
+
+    int payload_size_;
+    std::vector<int> pre_generated_indices_;
+    int cur_idx_;
+
+    void init_(int pre_indices_size);
+};
+
 }
 }
