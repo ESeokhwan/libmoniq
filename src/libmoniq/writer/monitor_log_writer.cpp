4be73ff0e7a71ac57c69299926241de91afdb2c1
eseokhwan <huan99@hanmail.net>
2026-02-02 14:42:27 +0900

    feat: add multi worker on writer
    feat: add multi worker on writer

--- src/libmoniq/writer/monitor_log_writer.cpp
+++ src/libmoniq/writer/monitor_log_writer.cpp
@@ -1,4 +1,7 @@
 #include "libmoniq/writer/monitor_log_writer.h"
+#include <atomic>
+#include <memory>
+#include <thread>
 
 namespace moniq {
 namespace writer {
@@ -11,19 +14,35 @@ MonitorLogWriter::MonitorLogWriter(
     : monitor_queue_(monitor_queue),
       write_strategy_(write_strategy),
       batch_size_(batch_size),
-      timeout_(timeout)
+      timeout_(timeout), 
+      worker_cnt_(1)
+{}
+
+MonitorLogWriter::MonitorLogWriter(
+    std::shared_ptr<MonitorQueue> monitor_queue,
+    std::shared_ptr<IMonitorLogWriteStrategy> write_strategy,
+    int batch_size,
+    int timeout,
+    int worker_cnt)
+    : monitor_queue_(monitor_queue),
+      write_strategy_(write_strategy),
+      batch_size_(batch_size),
+      timeout_(timeout), 
+      worker_cnt_(worker_cnt)
 {}
 
 void MonitorLogWriter::run() {
     while (!(terminated_.load(std::memory_order_relaxed) && monitor_queue_->is_empty())) {
         synced_wait();
         int processed_batch_size = batch_size_ < 0 ? monitor_queue_->size() : batch_size_;
-        for (int i = 0; i < processed_batch_size; i++) {
-            std::unique_ptr<IMonitorLog> log_qp = monitor_queue_->dequeue();
-            if (log_qp == nullptr) break;
-            log_qp->preprocess();
-            write_strategy_->write(std::move(log_qp));
+        if (processed_batch_size == 0) continue;
+
+        processed_.store(0, std::memory_order_relaxed);
+        std::vector<std::thread> workers;
+        for (int i = 0; i < worker_cnt_; i++) {
+            workers.emplace_back(std::thread(&MonitorLogWriter::process_write, this, processed_batch_size));
         }
+        for (auto& worker : workers) worker.join();
         write_strategy_->commit();
     }
 }
@@ -37,6 +56,23 @@ void MonitorLogWriter::notify_if_needed() {
 void MonitorLogWriter::graceful_shutdown() {
     terminated_.store(true, std::memory_order_relaxed);
     synced_notify();
+}
+
+void MonitorLogWriter::process_write(int todo_cnt) {
+    int cur_processed_ = processed_.fetch_add(1, std::memory_order_relaxed);
+    while (cur_processed_ < todo_cnt) {
+        try {
+            std::unique_ptr<IMonitorLog> log_qp;
+            log_qp = monitor_queue_->dequeue();
+            if (log_qp == nullptr) {
+                processed_.store(todo_cnt, std::memory_order_relaxed);
+                break;
+            }
+            log_qp->preprocess();
+            write_strategy_->write(std::move(log_qp));
+        } catch (const std::exception& e) {}
+        cur_processed_ = processed_.fetch_add(1, std::memory_order_relaxed);
+    }
 }
 
 void MonitorLogWriter::synced_wait() {
--- src/libmoniq/writer/monitor_log_writer.h
+++ src/libmoniq/writer/monitor_log_writer.h
@@ -19,11 +19,20 @@ public:
         int timeout
     );
 
+    MonitorLogWriter(
+        std::shared_ptr<MonitorQueue> monitor_queue,
+        std::shared_ptr<IMonitorLogWriteStrategy> write_strategy,
+        int batch_size,
+        int timeout,
+        int worker_cnt
+    );
+
     void run();
     void notify_if_needed();
     void graceful_shutdown();
 
 private:
+    void process_write(int todo_cnt);
     void synced_wait();
     void synced_notify();
     bool is_batch_full();
@@ -32,8 +41,10 @@ private:
     std::shared_ptr<IMonitorLogWriteStrategy> write_strategy_;
     int batch_size_;
     int timeout_;
+    int worker_cnt_;
 
     std::atomic<bool> terminated_{false};
+    std::atomic<int> processed_{0};
 
     std::mutex mtx_;
     std::condition_variable cv_;
